d4e3856146650023b7e89fa9df5daf717ec0a2c2
Randeep Singh <71288034+RaSingh02@users.noreply.github.com>
2022-01-22 16:31:17 -0500

    Add background img logic, added more quotes, added styling, added animations

--- js/main.js
+++ js/main.js
@@ -1,5 +1,63 @@
+let prevAuthor = null;
 function generate() {
     let randNum = Math.floor(Math.random() * quotesArr.length);
+    sessionStorage.setItem(document.querySelector("blockquote").innerText, document.querySelector("cite").innerText);
+    prevQuote = document.querySelector("blockquote").innerText;
+    prevAuthor = document.querySelector("cite").innerText;
     document.querySelector("blockquote").innerText = quotesArr[randNum].quote;
-    document.querySelector("cite").innerText = quotesArr[randNum].author;   
+    document.querySelector("cite").innerText = quotesArr[randNum].author;
+    document.getElementById("background").classList.add("img");
+    sessionStorage.clear();
+}
+generate()
+
+function previous() { 
+    document.querySelector("blockquote").innerText = prevQuote;
+    document.querySelector("cite").innerText = prevAuthor;   
+}
+
+function gen_background() {
+    let bgNum = Math.floor(Math.random() * 12);
+    document.getElementById("background").setAttribute("class", "")
+
+    switch (bgNum) {
+        case 1:
+            document.getElementById("background").classList.add("img1");
+            break;
+        case 2:
+            document.getElementById("background").classList.add("img2");
+            break;
+        case 3:
+            document.getElementById("background").classList.add("img3");
+            break;
+        case 4:
+            document.getElementById("background").classList.add("img4");
+            break;
+        case 5:
+            document.getElementById("background").classList.add("img5");
+            break;
+        case 6:
+            document.getElementById("background").classList.add("img6");
+            break;
+        case 7:
+            document.getElementById("background").classList.add("img7");
+            break;
+        case 8:
+            document.getElementById("background").classList.add("img8");
+            break;
+        case 9:
+            document.getElementById("background").classList.add("img9");
+            break;
+        case 10:
+            document.getElementById("background").classList.add("img10");
+            break;
+        case 11:
+            document.getElementById("background").classList.add("img11");
+            break;
+        case 12:
+            document.getElementById("background").classList.add("img12");
+            break;
+        default:
+            break;
+    }
 }
--- js/quotes.js
+++ js/quotes.js
@@ -1,26 +1,50 @@
 const quotesArr = [
     {
         "quote": "The bright day is done, And we are for the dark.",
-        "author": "William Shakespeare"
+        "author": "-William Shakespeare"
     },
     {
         "quote": "The only way to begin is to stop talking and start doing.",
-        "author": "Walter Elias Disney"
+        "author": "-Walter Elias Disney"
     },
     {
         "quote": "How did it get so late so soon.",
-        "author": "Dr. Seuss"
+        "author": "-Dr. Seuss"
     },
     {
         "quote": "Every time I plant a seed, He say kill it before it grow, he say kill it before they grow.",
-        "author": "Bob Marley"
+        "author": "-Bob Marley"
     },
     {
-        "quote": "Hitler liked to spend a lot of special time with goats.",
-        "author": "Dwight D. Eisenhower"
+        "quote": "I believe that everything happens for a reason. People change so that you can learn to let go, things go wrong so that you appreciate them when they're right, you believe lies so you eventually learn to trust no one but yourself, and sometimes good things fall apart so better things can fall together.",
+        "author": "-Dwight D. Eisenhower"
     },
     {
-        "quote": "Amogus",
-        "author": "Gamers"
+        "quote": "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+        "author": "―Marilyn Monroe"
+    },
+    {
+        "quote": "So many books, so little time.",
+        "author": "-Frank Zappa"
+    },
+    {
+        "quote": "A room without books is like a body without a soul.",
+        "author": "-Marcus Tullius Cicero"
+    },
+    {
+        "quote": "You only live once, but if you do it right, once is enough.",
+        "author": "-Mae West"
+    },
+    {
+        "quote": "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
+        "author": "-J.K. Rowling, Harry Potter and the Goblet of Fire"
+    },
+    {
+        "quote": "A friend is someone who knows all about you and still loves you.",
+        "author": "-Elbert Hubbard"
+    },
+    {
+        "quote": "Without music, life would be a mistake.",
+        "author": "-Friedrich Nietzsche, Twilight of the Idols"
     }
 ];
